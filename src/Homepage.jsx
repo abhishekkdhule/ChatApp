cd25008600cb8b6f602af2a2211e3afb882397ae
abhishek <abhishek.dhule.79@gmail.com>
2021-08-03 21:02:32 +0530

    button transition added

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -4,12 +4,10 @@ import styles from './Homepage.module.css'
 function Homepage() {
 
     const [roomId, setRoomId] = useState('')
-    const [createRoomBtnStyle, setcreateRoomBtnStyle] = useState(styles.create_room_button)
+    const [hompageStyles, sethompageStyles] = useState({input_box:styles.initial_display ,closingBtn:styles.create_room_button, openingBtn:styles.initial_display})
 
-    const createRoom = () =>{
-        setcreateRoomBtnStyle({
-            
-        })
+    const createRoom = (e) =>{
+        sethompageStyles({input_box:styles.upper_div ,closingBtn: styles.create_room_button + " " + styles.close_btn, openingBtn: styles.enter_btn})
     }
 
     const joinRoom = () =>{
@@ -19,14 +17,18 @@ function Homepage() {
     return (
         <>
             <div className={styles.main}>
-                <div className={styles.upper_div}>
-                    <input placeholder="Room Id" value={roomId} onChange={(e)=>setRoomId(e.target.value)}/>
+                <div className={hompageStyles.input_box}>
+                    <input placeholder="Your Name" value={roomId} onChange={(e)=>setRoomId(e.target.value)}/>
+                    <input placeholder="Room ID" value={roomId} onChange={(e)=>setRoomId(e.target.value)}/>
                 </div>
                 <div className={styles.lower_div}>        
-                    <button className={createRoomBtnStyle} onClick={()=>createRoom()}>
+                    <button className={hompageStyles.closingBtn} onClick={(e)=>createRoom(e.target)}>
                         CREATE ROOM
                     </button>
-                    <button className={styles.join_room_button} onClick={()=>joinRoom()}>
+                    <button className={hompageStyles.openingBtn} onClick={(e)=>createRoom(e.target)}>
+                        ENTER
+                    </button>
+                    <button className={hompageStyles.closingBtn} onClick={()=>joinRoom()}>
                         JOIN ROOM
                     </button>
                 </div>
