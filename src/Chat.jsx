a0832ae7c8524e65398b6b6d8dfff14fb8d70d77
Abhishek Dhule <abhishek.dhule.79@gmail>
2021-08-25 23:51:58 +0530

    message updates

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -5,14 +5,14 @@ import {firebase,db} from './firebase';
 
 function Chat() {
     const [messages, setMessages] = useState([])
-    const [newMessage, setNewMessage] = useState([])
+    const [newMessage, setNewMessage] = useState("")
     const [userdetails, setUserDetails] = useState({})
 
     useEffect(()=>{    
         setUserDetails({"name": localStorage.getItem("name"),
                         "room_id": localStorage.getItem("room_id")})
-        console.log(userdetails)
-        db.collection("rooms").where("room_id", "==", userdetails.room_id).get().then((querySnapshot) => {
+       
+        db.collection("rooms").where("room_id", "==", localStorage.getItem("room_id")).get().then((querySnapshot) => {
             querySnapshot.forEach((doc) => {
                 // doc.data() is never undefined for query doc snapshots
                 // console.log(doc.id, " => ", doc.data().messages);
@@ -22,18 +22,32 @@ function Chat() {
     },[])
     
 
+    const sendMessage = (e) =>{
+        setMessages([...messages, {"name":userdetails.name, "message":newMessage}])
+        setNewMessage("")
+        console.log(...messages, {"name":userdetails.name, "message":newMessage} )
+        e.preventDefault()
+        let doc = db.collection("rooms").where("room_id", "==", userdetails.room_id).get()
+        console.log(doc)
+    }
+
     return (
         <>
             <h4>Room</h4>
-            <div className="chat">
-            {
-                messages.map((m,messages) => {
-                    return (
-                        <Message message={m}/>
-                    )
-                } )
-            }
-            <input className="chatText" type="text" value={newMessage} onChange={(e)=>setNewMessage(e.target.value)}/>
+            <div className="main_chat">
+                <div className="chat">
+                {
+                    messages.map((m,messages) => {
+                        return (
+                            <Message message={m}/>
+                        )
+                    } )
+                }
+                
+                </div>
+                <form onSubmit={sendMessage}>
+                    <input className="chatText" type="text" value={newMessage} onChange={(e)=>setNewMessage(e.target.value)}/>
+                </form>
             </div>
         </>
     )
